16b04f7be739852c99e378c8645b6c6352e2bb5f
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-26 13:53:25 -0300

    feat(controller): add error handling middleware for FastAPI

--- src/application/controllers/__init__.py
+++ src/application/controllers/__init__.py
@@ -4,32 +4,34 @@ from fastapi.responses import JSONResponse
 from src.application.http import UnprocessableEntity
 from src.application.usecases.get_stock import GetStock
 from src.application.usecases.purchase_stock import PurchaseStock
+from src.infra.cache.in_memory_stocks_proxy import InMemoryStocksProxy
 from src.infra.dao.in_memory_stocks_dao import InMemoryStocksDAO
 from src.infra.gateways import StocksGatewayAdapter
 
 router = APIRouter()
 
-stocks_gateway = StocksGatewayAdapter()
+stocks_gateway = InMemoryStocksProxy(StocksGatewayAdapter())
 stocks_dao = InMemoryStocksDAO()
+
 
 @router.get("/stocks/{symbol}")
 async def get_stocks(symbol):
-  
-  usecase = GetStock(stocks_gateway, stocks_dao)
-  
-  response = usecase.execute(symbol.upper())
-  return response
+
+    usecase = GetStock(stocks_gateway, stocks_dao)
+
+    response = usecase.execute(symbol.upper())
+    return response
 
 
 @router.post("/stocks/{symbol}")
-async def post_stocks(request: Request,symbol):
-  body = await request.json()
-  amount = body.get("amount")
-  
-  if not amount:
-    raise UnprocessableEntity("Amount is required")
-  
-  usecase = PurchaseStock(stocks_dao)
-  
-  response = usecase.execute(symbol.upper(), amount)
-  return JSONResponse(status_code=201, content=response)
+async def post_stocks(request: Request, symbol):
+    body = await request.json()
+    amount = body.get("amount")
+
+    if not amount:
+        raise UnprocessableEntity("Amount is required")
+
+    usecase = PurchaseStock(stocks_dao)
+
+    response = usecase.execute(symbol.upper(), amount)
+    return JSONResponse(status_code=201, content=response)
