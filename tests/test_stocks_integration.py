5170943b2910390510db1cd811e5dd066ccfd414
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-26 13:53:25 -0300

    feat(test): add test cases for StocksController

--- tests/test_stocks_integration.py
+++ tests/test_stocks_integration.py
@@ -17,8 +17,9 @@ def client():
     app.include_router(router)
     yield TestClient(app)
 
+
 def test_get_stock_from_api_scraping_and_database(client):
-    
+
     response = client.get("/stocks/AAPL")
 
     assert [
@@ -36,26 +37,26 @@ def test_get_stock_from_api_scraping_and_database(client):
         "amount",
     ] == list(response.json().keys())
 
+
 def test_it_should_get_the_saved_value(client):
-    
+
     amount = 10
     client.post("/stocks/AAPL", json={"amount": amount})
     response = client.get("/stocks/AAPL")
 
     assert response.json()["amount"] == amount
 
+
 def test_it_should_return_201_if_amount_is_present(client):
-    
+
     amount = 10
     response = client.post("/stocks/AAPL", json={"amount": amount})
 
     assert response.status_code == 201
 
+
 def test_it_should_return_422_if_amount_is_missing(client):
-    
+
     response = client.post("/stocks/AAPL", json={})
 
     assert response.status_code == 422
-    
-
-
